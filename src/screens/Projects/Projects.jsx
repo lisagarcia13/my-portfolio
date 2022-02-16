58b4e388602ba33ac665fda99731a5bbf208db5c
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-02-16 11:01:31 -0600

    grid on project page

--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -6,67 +6,120 @@ export default function Projects() {
   return (
     <div>
       <div className="pocketdex">
-        <h2 className="project-name">PocketDex</h2>
-        <img className="pocketdex-pic" src={name} alt="pokemon" />
-        <p className="project-info">
-          PocketDex is a pokedex that was created using HTML, CSS and vanilla
-          JS. The data was gathered using the Pokemon API.
-        </p>
-        <a href="https://lisagarcia13.github.io/PocketDex/">Live Site</a>
-        <br />
-        <a href="https://github.com/lisagarcia13/PocketDex">Github</a>
+        <div>
+          <img className="pocketdex-pic" src={name} alt="pokemon" />
+        </div>
+        <div>
+          <h2 className="project-name">PocketDex</h2>
+
+          <p className="project-info">
+            PocketDex is a pokedex that was created using HTML, CSS and vanilla
+            JS. The data was gathered using the Pokemon API.
+          </p>
+          <br />
+          <a
+            href="https://lisagarcia13.github.io/PocketDex/"
+            className="link-project"
+          >
+            Live Site
+          </a>
+
+          <a
+            href="https://github.com/lisagarcia13/PocketDex"
+            className="link-project"
+          >
+            Github
+          </a>
+        </div>
       </div>
 
       <div className="salsa">
-        <h2 className="project-name">What's in the Salsa?</h2>
-        <img
-          className="salsa-pic"
-          src="https://cdn.pixabay.com/photo/2015/03/06/23/35/pepper-662550_960_720.jpg"
-          alt="peppers"
-        />
-        <p className="project-info">
-          An app that is built using React, Airtable, and MUI that allows users
-          to blog and share salsa recipes.Users will be able to see salsa's in
-          their respective catergories of mild, medium and hot.
-        </p>
-        <a href="https://sleepy-gates-e6ddfb.netlify.app/">Live Site</a>
-        <br />
-        <a href="https://github.com/lisagarcia13/whats-in-the-salsa">Github</a>
+        <div>
+          <img
+            className="salsa-pic"
+            src="https://cdn.pixabay.com/photo/2015/03/06/23/35/pepper-662550_960_720.jpg"
+            alt="peppers"
+          />
+        </div>
+        <div>
+          <h2 className="project-name">What's in the Salsa?</h2>
+
+          <p className="project-info">
+            An app that is built using React, Airtable, and MUI that allows
+            users to blog and share salsa recipes.Users will be able to see
+            salsa's in their respective catergories of mild, medium and hot.
+          </p>
+          <br />
+          <a
+            href="https://sleepy-gates-e6ddfb.netlify.app/"
+            className="link-project"
+          >
+            Live Site
+          </a>
+
+          <a
+            href="https://github.com/lisagarcia13/whats-in-the-salsa"
+            className="link-project"
+          >
+            Github
+          </a>
+        </div>
       </div>
 
       <div className="rnlh">
-        <h2 className="project-name">RNLH</h2>
-        <img
-          className="rnlh-pic"
-          src="https://res.cloudinary.com/dojhf40bp/image/upload/v1641316329/apparel-ga341b623f_1920_bomxmz.jpg"
-          alt="rnlh logo"
-        />
-        <p className="project-info">
-          A full stack website built together in a group. Built using React on
-          frontend and MongoDB on the backend. Users can create accounts and
-          post their listing of items they want to sell.
-        </p>
+        <div>
+          <img
+            className="rnlh-pic"
+            src="https://res.cloudinary.com/dojhf40bp/image/upload/v1641316329/apparel-ga341b623f_1920_bomxmz.jpg"
+            alt="rnlh logo"
+          />
+        </div>
+        <div>
+          <h2 className="project-name">RNLH</h2>
 
-        <a href="https://rnlh.netlify.app">Live Site</a>
-        <br />
-        <a href="https://github.com/rodennis/RNLH">Github</a>
+          <p className="project-info">
+            A full stack website built together in a group. Built using React on
+            frontend and MongoDB on the backend. Users can create accounts and
+            post their listing of items they want to sell.
+          </p>
+          <br />
+          <a href="https://rnlh.netlify.app" className="link-project">
+            Live Site
+          </a>
+          <a href="https://github.com/rodennis/RNLH" className="link-project">
+            Github
+          </a>
+        </div>
       </div>
       <div className="greatreads">
-        <h2 className="project-name">GreatReads</h2>
-        <img
-          className="greatreads-pic"
-          src="https://cdn.pixabay.com/photo/2017/03/17/10/29/coffee-2151200_1280.jpg"
-          alt="coffee and book"
-        />
-        <p className="project-info">
-          GreatReads is a website where users can lookup books and authors. It
-          is a fullstack website that has React on frontend and Django REST on
-          backend. It has full CRUD functionality for both authors and books
-          once users are logged in.
-        </p>
-        <a href="https://great-reads.netlify.app/">Live Site</a>
-        <br />
-        <a href="https://github.com/lisagarcia13/great-reads">Github</a>
+        <div>
+          <img
+            className="greatreads-pic"
+            src="https://cdn.pixabay.com/photo/2017/03/17/10/29/coffee-2151200_1280.jpg"
+            alt="coffee and book"
+          />
+        </div>
+        <div>
+          <h2 className="project-name">GreatReads</h2>
+
+          <p className="project-info">
+            GreatReads is a website where users can lookup books and authors. It
+            is a fullstack website that has React on frontend and Django REST on
+            backend. It has full CRUD functionality for both authors and books
+            once users are logged in.
+          </p>
+          <br />
+          <a href="https://great-reads.netlify.app/" className="link-project">
+            Live Site
+          </a>
+
+          <a
+            href="https://github.com/lisagarcia13/great-reads"
+            className="link-project"
+          >
+            Github
+          </a>
+        </div>
       </div>
     </div>
   );
