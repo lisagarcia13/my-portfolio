dff7e942f3b1bd56ad329c65bd2a191b42361504
lisagarcia3 <lisa@Lisas-Air.attlocal.net>
2022-02-16 19:45:40 -0600

    added gif to homepage

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Lisa Garcia</h1>
-      <h3> Software Engineer</h3>
+    <div className="home-container">
+      <h1 className="home-header">Lisa Garcia</h1>
+      <h3 className="home-subheader"> Software Engineer</h3>
+      <div>
+        <p>
+          Feel free to browse through my work and don't forget to connect with
+          me through my contact page.
+        </p>
+        <img
+          src="https://eportfolio.utm.my/artefact/file/download.php?file=540089&view=116143"
+          alt="welcome"
+          className="welcome"
+        />
+      </div>
+      <Link to="/projects" style={{ textDecoration: "none" }}>
+        {" "}
+        <h3 className="home-link">View my work!</h3>
+      </Link>
     </div>
   );
 }
